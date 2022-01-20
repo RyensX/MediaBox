afb64696bce548e60557ed8f40828bd67ceff6e4
Ryensu <ryensu@qq.com>
2022-01-21 00:23:21 +0800

    [feature]App支持记忆播放进度

---
+++ app/src/main/java/com/skyd/imomoe/database/dao/PlayRecordDao.kt
@@ -0,0 +1,20 @@
+package com.skyd.imomoe.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.skyd.imomoe.config.Constant
+import com.skyd.imomoe.database.entity.PlayRecordEntity
+
+@Dao
+interface PlayRecordDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(vararg record: PlayRecordEntity)
+
+    @Query("SELECT * FROM ${Constant.Database.OfflineData.PLAY_RECORD_TABLE_NAME} WHERE url=:url")
+    suspend fun query(url: String): PlayRecordEntity?
+
+    @Query("DELETE FROM ${Constant.Database.OfflineData.PLAY_RECORD_TABLE_NAME} WHERE url = :url")
+    fun delete(url: String)
+}
--- app/src/main/java/com/skyd/imomoe/util/AnimeVideoPositionMemoryStore.kt
+++ app/src/main/java/com/skyd/imomoe/util/AnimeVideoPositionMemoryStore.kt
@@ -1,26 +1,19 @@
 package com.skyd.imomoe.util
 
+import com.skyd.imomoe.database.entity.PlayRecordEntity
+import com.skyd.imomoe.database.getOfflineDatabase
 import com.skyd.imomoe.view.component.player.AnimeVideoPlayer
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
-//test
 object AnimeVideoPositionMemoryStore : AnimeVideoPlayer.PlayPositionMemoryDataStore {
 
-    private val map = mutableMapOf<String, Long>()
+    private val dao = getOfflineDatabase().playPlayRecordDao()
 
-    override suspend fun getPlayPosition(url: String): Long? {
-        withContext(Dispatchers.Main) {
-            "获取播放进度".showToast()
-        }
-        return map[url]
-    }
+    override suspend fun getPlayPosition(url: String): Long? = dao.query(url)?.position
 
     override suspend fun putPlayPosition(url: String, position: Long) {
-        withContext(Dispatchers.Main) {
-            "记忆播放进度".showToast()
-        }
-        map[url] = position
+        dao.insert(PlayRecordEntity(url, position))
     }
 
     override fun positionFormat(position: Long): String = position.toString()
