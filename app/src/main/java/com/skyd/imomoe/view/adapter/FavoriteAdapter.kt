8bc3b1135874be74c8c28331e05db441084d4d7b
Ryensu <ryensu@qq.com>
2022-01-22 16:21:59 +0800

    [feature|optimize]收藏（我的番剧）长按跳转番剧详情；优化点击绑定

--- app/src/main/java/com/skyd/imomoe/view/adapter/FavoriteAdapter.kt
+++ app/src/main/java/com/skyd/imomoe/view/adapter/FavoriteAdapter.kt
@@ -1,6 +1,7 @@
 package com.skyd.imomoe.view.adapter
 
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.skyd.imomoe.App
 import com.skyd.imomoe.R
@@ -18,6 +19,33 @@ class FavoriteAdapter(
     private val dataList: List<FavoriteAnimeBean>
 ) : BaseRvAdapter(dataList) {
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return super.onCreateViewHolder(parent, viewType).also {
+            when (it) {
+                is AnimeCover8ViewHolder -> {
+                    //点击
+                    it.itemView.setOnClickListener { _ ->
+                        val item = dataList[it.bindingAdapterPosition]
+                        if (item.lastEpisodeUrl != null)
+                            process(
+                                activity,
+                                item.lastEpisodeUrl + item.animeUrl,
+                                item.lastEpisodeUrl ?: ""
+                            )
+                        else
+                            process(activity, item.animeUrl, item.animeUrl)
+                    }
+                    //长度跳转详情页
+                    it.itemView.setOnLongClickListener { _ ->
+                        val item = dataList[it.bindingAdapterPosition]
+                        process(activity, item.animeUrl, item.animeUrl)
+                        true
+                    }
+                }
+            }
+        }
+    }
+
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
         val item = dataList[position]
@@ -34,16 +62,6 @@ class FavoriteAdapter(
                 } else {
                     holder.tvAnimeCover8Episodes.visible()
                     holder.tvAnimeCover8Episodes.text = item.lastEpisode
-                }
-                holder.itemView.setOnClickListener {
-                    if (item.lastEpisodeUrl != null)
-                        process(
-                            activity,
-                            item.lastEpisodeUrl + item.animeUrl,
-                            item.lastEpisodeUrl ?: ""
-                        )
-                    else
-                        process(activity, item.animeUrl, item.animeUrl)
                 }
             }
             else -> {
