949fd6cd914913f5c29beaf2b89f2166fa2afd5e
Ryensu <ryensu@qq.com>
2022-02-17 12:00:28 +0800

    [fix]移除Shortcuts

--- app/src/main/java/com/su/mediabox/view/activity/MainActivity.kt
+++ app/src/main/java/com/su/mediabox/view/activity/MainActivity.kt
@@ -119,52 +119,6 @@ class MainActivity : BasePluginActivity<ActivityMainBinding>(), EventBusSubscrib
                 setTabSelection(2)
             }
         }
-
-        registerShortcuts()
-    }
-
-    /**
-     *
-     * 设置app图标快捷菜单
-     */
-    private fun registerShortcuts() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
-            val mShortcutManager = getSystemService(ShortcutManager::class.java)
-            val shortcutInfoList = listOf(
-                ShortcutInfo.Builder(this, ID_FAVORITE)
-                    .setShortLabel(getString(R.string.shortcuts_favorite_short))
-                    .setLongLabel(getString(R.string.shortcuts_favorite_long))
-                    .setIcon(
-                        Icon.createWithResource(this, R.drawable.layerlist_shortcuts_favorite_24)
-                    )
-                    .setIntent(
-                        Intent(this, FavoriteActivity::class.java).setAction(Intent.ACTION_VIEW)
-                    )
-                    .build(),
-                ShortcutInfo.Builder(this, ID_EVERYDAY)
-                    .setShortLabel(getString(R.string.shortcuts_everyday_short))
-                    .setLongLabel(getString(R.string.shortcuts_everyday_long))
-                    .setIcon(
-                        Icon.createWithResource(this, R.drawable.layerlist_shortcuts_everyday_24)
-                    )
-                    .setIntent(
-                        Intent(this, MainActivity::class.java).setAction(ACTION_EVERYDAY)
-                    )
-                    .build(),
-                ShortcutInfo.Builder(this, ID_DOWNLOAD)
-                    .setShortLabel(getString(R.string.shortcuts_download_short))
-                    .setLongLabel(getString(R.string.shortcuts_download_long))
-                    .setIcon(
-                        Icon.createWithResource(this, R.drawable.layerlist_shortcuts_download_24)
-                    )
-                    .setIntent(
-                        Intent(this, AnimeDownloadActivity::class.java)
-                            .setAction(Intent.ACTION_VIEW)
-                    )
-                    .build()
-            )
-            mShortcutManager.dynamicShortcuts = shortcutInfoList
-        }
     }
 
     override fun getBinding(): ActivityMainBinding = ActivityMainBinding.inflate(layoutInflater)
