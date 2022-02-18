cc0a244048a7ecc23890d6c9820a68b596af1cfc
Ryensu <ryensu@qq.com>
2022-02-18 13:04:19 +0800

    [optimize]移除根据网页跳转（路由逻辑不再由插件控制）

--- app/src/main/java/com/su/mediabox/view/fragment/MoreFragment.kt
+++ app/src/main/java/com/su/mediabox/view/fragment/MoreFragment.kt
@@ -8,10 +8,7 @@ import com.su.mediabox.R
 import com.su.mediabox.bean.MoreBean
 import com.su.mediabox.pluginapi.Constant.ViewHolderTypeString
 import com.su.mediabox.databinding.FragmentMoreBinding
-import com.su.mediabox.view.activity.AboutActivity
 import com.su.mediabox.view.activity.HistoryActivity
-import com.su.mediabox.view.activity.SettingActivity
-import com.su.mediabox.view.activity.SkinActivity
 import com.su.mediabox.view.adapter.MoreAdapter
 import com.su.mediabox.pluginapi.Constant.ActionUrl
 import com.su.mediabox.pluginapi.Text.buildRouteActionUrl
@@ -35,14 +32,6 @@ class MoreFragment : BaseFragment<FragmentMoreBinding>() {
                 ),
                 getString(R.string.watch_history),
                 R.drawable.ic_history_white_24
-            )
-        )
-        list.add(
-            MoreBean(
-                ViewHolderTypeString.MORE_1,
-                ActionUrl.ANIME_SKIP_BY_WEBSITE,
-                getString(R.string.skip_by_website),
-                R.drawable.ic_insert_link_white_24
             )
         )
 
