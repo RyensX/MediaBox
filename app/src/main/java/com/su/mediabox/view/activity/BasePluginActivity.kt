f9c40ecc762e3d625b978c41e7a6948702b5dd93
Ryensu <ryensu@qq.com>
2022-02-18 19:48:32 +0800

    [optimize]PluginActivity传染信息由VM持久化防止系统回收后重建丢失插件绑定
    close #3

--- app/src/main/java/com/su/mediabox/view/activity/BasePluginActivity.kt
+++ app/src/main/java/com/su/mediabox/view/activity/BasePluginActivity.kt
@@ -2,6 +2,8 @@ package com.su.mediabox.view.activity
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 import com.su.mediabox.AppRouteProcessor
 import com.su.mediabox.PluginManager.getPluginIndex
@@ -12,6 +14,8 @@ abstract class BasePluginActivity<VB : ViewBinding> : BaseActivity<VB>() {
     companion object {
         const val PLUGIN_INFO_INDEX = "pluginIndex"
     }
+
+    private val pluginInfoViewModel by viewModels<PluginInfoViewModel>()
 
     /**
      * 传染式传递当前所属插件
@@ -32,11 +36,22 @@ abstract class BasePluginActivity<VB : ViewBinding> : BaseActivity<VB>() {
         super.onCreate(savedInstanceState)
         //接入路由控制
         AppRouteProcessor.init(this)
+        //在重建时恢复数据
+        val index = getPluginIndex()
+        if (index == -1)
+            intent.setPluginInfo(pluginInfoViewModel.index)
+        else
+            pluginInfoViewModel.index = index
     }
 
     override fun onResume() {
         super.onResume()
         //接入路由控制
         AppRouteProcessor.updateTarget(this)
+    }
+
+    class PluginInfoViewModel : ViewModel() {
+        //虽然目前只是简单的数据
+        var index: Int = -1
     }
 }
