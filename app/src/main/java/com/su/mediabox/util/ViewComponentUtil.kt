a0ecf8ecbe4d3cd62e78b3af0ecacd246eaa5242
RyensX <ryensu@qq.com>
2022-04-02 18:27:37 +0800

    [fix]修复播放列表视图组件因为时序问题只根据历史播放定位

--- app/src/main/java/com/su/mediabox/util/ViewComponentUtil.kt
+++ app/src/main/java/com/su/mediabox/util/ViewComponentUtil.kt
@@ -3,11 +3,15 @@ package com.su.mediabox.util
 import androidx.activity.ComponentActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import com.su.mediabox.bean.HistoryBean
 import com.su.mediabox.database.getAppDataBase
 import com.su.mediabox.plugin.AppRouteProcessor
 import com.su.mediabox.v2.view.activity.VideoDetailActivity
 import com.su.mediabox.v2.viewmodel.VideoDetailViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 fun bindHistoryPlayInfo(
     observer: Observer<HistoryBean?>
@@ -19,13 +23,17 @@ fun bindHistoryPlayInfo(
             else -> null
         }
     } ?: return
-    //优先绑定收藏的历史播放记录
-    val lv = getAppDataBase().favoriteAnimeDao()
-        .getFavoriteAnimeLiveData(detailUrl)
-    if (lv.value == null)
+    context.lifecycleScope.launch(Dispatchers.Main) {
+        //优先绑定收藏的历史播放记录
+        withContext(Dispatchers.IO) {
+            getAppDataBase().favoriteAnimeDao().getFavoriteAnime(detailUrl)
+        }?.also {
+            getAppDataBase().favoriteAnimeDao()
+                .getFavoriteAnimeLiveData(detailUrl).observe(context, observer)
+            return@launch
+        }
         getAppDataBase().historyDao()
             .getHistoryLiveData(detailUrl)
             .observe(context, observer)
-    else
-        lv.observe(context, observer)
+    }
 }
