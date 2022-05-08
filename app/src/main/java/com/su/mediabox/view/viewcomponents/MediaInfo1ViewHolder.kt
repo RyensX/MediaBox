c4a39765a3f62ea36d6beb7753d28580fbcdace7
RyensX <ryensu@qq.com>
2022-05-08 20:20:54 +0800

    [feature]媒体信息样式1视图组件支持控制封面高度及其他数据颜色

--- app/src/main/java/com/su/mediabox/view/viewcomponents/MediaInfo1ViewHolder.kt
+++ app/src/main/java/com/su/mediabox/view/viewcomponents/MediaInfo1ViewHolder.kt
@@ -6,11 +6,9 @@ import android.view.ViewGroup
 import com.su.mediabox.R
 import com.su.mediabox.databinding.ViewComponentMeidaInfo1Binding
 import com.su.mediabox.pluginapi.data.MediaInfo1Data
+import com.su.mediabox.util.*
 import com.su.mediabox.util.Util.getResColor
 import com.su.mediabox.util.coil.CoilUtil.loadImage
-import com.su.mediabox.util.gone
-import com.su.mediabox.util.setOnClickListener
-import com.su.mediabox.util.visible
 import com.su.mediabox.view.activity.MediaDetailActivity
 import com.su.mediabox.view.adapter.type.TypeViewHolder
 
@@ -21,6 +19,7 @@ class MediaInfo1ViewHolder private constructor(private val binding: ViewComponen
     TypeViewHolder<MediaInfo1Data>(binding.root) {
 
     private val nameColor = binding.root.context.resources.getColor(R.color.foreground_black_skin)
+    protected val styleColor = getResColor(R.color.main_color_2_skin)
 
     constructor(parent: ViewGroup) : this(
         ViewComponentMeidaInfo1Binding.inflate(
@@ -36,27 +35,26 @@ class MediaInfo1ViewHolder private constructor(private val binding: ViewComponen
         }
     }
 
-    override fun onBind(dataMedia: MediaInfo1Data) {
-        super.onBind(dataMedia)
-        binding.vcGirdMediaItemTitle.apply {
+    override fun onBind(data: MediaInfo1Data) {
+        super.onBind(data)
+        binding.vcGirdMediaItemTitle.displayOnlyIfHasData(data.name) {
             //自适应颜色，只有在详情页才需要是白色，其余的根据当前主题自动选择
             if (itemView.context.javaClass != MediaDetailActivity::class.java)
                 setTextColor(nameColor)
             else
                 setTextColor(Color.WHITE)
-            text = dataMedia.name
+            text = data.name
         }
         //TODO 滚动延迟加载
-        binding.vcGirdMediaItemCover.loadImage(dataMedia.coverUrl)
-        binding.vcGirdMediaItemEpisode.apply {
-            val other = dataMedia.other
-            if (other.isBlank())
-                gone()
-            else {
-                visible()
-                setTextColor(getResColor(R.color.main_color_2_skin))
-                text = dataMedia.other
-            }
+        binding.vcGirdMediaItemCover.apply {
+            layoutParams.height = data.coverHeight
+            loadImage(data.coverUrl)
+        }
+        binding.vcGirdMediaItemEpisode.displayOnlyIfHasData(data.other) {
+            setTextColor(getResColor(R.color.main_color_2_skin))
+            text = data.other
+            val color = if (data.otherColor != -1) data.otherColor else styleColor
+            setTextColor(color)
         }
     }
 
