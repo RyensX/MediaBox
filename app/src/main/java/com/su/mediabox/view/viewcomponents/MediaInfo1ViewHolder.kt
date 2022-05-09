a7a980739adc8375d5ee3d7842b5ebb77e60f078
RyensX <ryensu@qq.com>
2022-05-09 21:26:22 +0800

    [feature]适配新增媒体信息样式1视图组件新配置项

--- app/src/main/java/com/su/mediabox/view/viewcomponents/MediaInfo1ViewHolder.kt
+++ app/src/main/java/com/su/mediabox/view/viewcomponents/MediaInfo1ViewHolder.kt
@@ -18,7 +18,6 @@ import com.su.mediabox.view.adapter.type.TypeViewHolder
 class MediaInfo1ViewHolder private constructor(private val binding: ViewComponentMeidaInfo1Binding) :
     TypeViewHolder<MediaInfo1Data>(binding.root) {
 
-    private val nameColor = binding.root.context.resources.getColor(R.color.foreground_black_skin)
     protected val styleColor = getResColor(R.color.main_color_2_skin)
 
     constructor(parent: ViewGroup) : this(
@@ -38,23 +37,27 @@ class MediaInfo1ViewHolder private constructor(private val binding: ViewComponen
     override fun onBind(data: MediaInfo1Data) {
         super.onBind(data)
         binding.vcGirdMediaItemTitle.displayOnlyIfHasData(data.name) {
-            //自适应颜色，只有在详情页才需要是白色，其余的根据当前主题自动选择
-            if (itemView.context.javaClass != MediaDetailActivity::class.java)
-                setTextColor(nameColor)
-            else
-                setTextColor(Color.WHITE)
-            text = data.name
+            gravity = data.gravity
+            val color = data.nameColor ?: styleColor
+            setTextColor(color)
+            if (text != data.name)
+                text = data.name
         }
         //TODO 滚动延迟加载
         binding.vcGirdMediaItemCover.apply {
+            scaleType = data.coverScaleType
             layoutParams.height = data.coverHeight
             loadImage(data.coverUrl)
         }
         binding.vcGirdMediaItemEpisode.displayOnlyIfHasData(data.other) {
-            setTextColor(getResColor(R.color.main_color_2_skin))
-            text = data.other
-            val color = if (data.otherColor != -1) data.otherColor else styleColor
+            gravity = data.gravity
+            val color = data.otherColor ?: styleColor
             setTextColor(color)
+            if (text != data.other)
+                text = data.other
+        }
+        binding.vcMediaInfo1Other2.displayOnlyIfHasData(data.other2) {
+            text = data.other2
         }
     }
 
